PixysOS/packages_apps_Settings

Incall vibration options [3/3]
*allow setting vibration when call is connected
*allow setting vibration when call is disconnected
*allow setting vibration for call waiting
*this works with google and aosp dialer :)

Change-Id: I1b99a40924406ec79e02e6f03fbe896f7cd8c83e

src/com/android/settings/fragments/SoundSettings.java
@@ -1,0 +1,37 @@
+package com.pixys.settings.fragments;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.os.Bundle;
+import com.android.settings.R;
+
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.settings.preferences.Utils;
+
+public class SoundSettings extends SettingsPreferenceFragment {
+
+    private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        addPreferencesFromResource(R.xml.sound_settings);
+
+        PreferenceScreen prefScreen = getPreferenceScreen();
+
+        PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS);
+        if (!Utils.isVoiceCapable(getActivity())) {
+            prefScreen.removePreference(incallVibCategory);
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.PIXYS_SETTINGS;
+    }
+}

taranhora/platform_packages_apps_Settings

src/com/android/settings/fragments/SoundSettings.java
@@ -1,0 +1,37 @@
+package com.lean.settings.fragments;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.os.Bundle;
+import com.android.settings.R;
+
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.settings.preferences.Utils;
+
+public class SoundSettings extends SettingsPreferenceFragment {
+
+    private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        addPreferencesFromResource(R.xml.sound_settings);
+
+        PreferenceScreen prefScreen = getPreferenceScreen();
+
+        PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS);
+        if (!Utils.isVoiceCapable(getActivity())) {
+            prefScreen.removePreference(incallVibCategory);
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.LEAN;
+    }
+}